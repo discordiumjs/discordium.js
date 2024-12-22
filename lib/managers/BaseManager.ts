ef28b849580c83526c49e7d19efdd139a2edc183
erffy <ceyhunerenerguven.07@proton.me>
2024-12-22 18:10:47 +0300

    Update TypeScript configuration and add BaseManager class
    - Modified tsconfig.json to include "DOM" in the lib array, added "esModuleInterop", "resolveJsonModule", "isolatedModules", and adjusted "noImplicitAny" to false for improved type handling.
    - Introduced a new abstract BaseManager class to manage API methods for data models, providing a structured way to handle client instantiation and method management.

---
+++ lib/managers/BaseManager.ts
@@ -0,0 +1,21 @@
+/**
+ * Manages the API methods of a data model.
+ * @abstract
+ */
+export default abstract class BaseManager {
+  /**
+   * The client that instantiated this Manager
+   * @name BaseManager#client
+   * @protected
+   * @readonly
+   */
+  protected readonly client: any;
+
+  /**
+   * Creates a new BaseManager instance
+   * @param client - The client that instantiates this Manager
+   */
+  public constructor(client: any) {
+    this.client = client;
+  }
+}
