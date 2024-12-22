8874290d85f7351a4d2e56858330df9e3bb3ed01
erffy <ceyhunerenerguven.07@proton.me>
2024-12-22 15:07:33 +0300

    Implement modal component management in ModalBuilder
    - Added methods for setting and adding components to the modal, enhancing the functionality of the ModalBuilder.
    - Introduced methods to set the custom ID and title of the modal, improving configurability.
    - Implemented a json() method to convert the modal to a JSON object for easier integration with Discord API interactions.

--- lib/structures/ModalBuilder.ts
+++ lib/structures/ModalBuilder.ts
@@ -1,7 +1,16 @@
-import { ModalBuilder } from '@discordjs/builders';
-import type { APIModalInteractionResponseCallbackData } from 'discord-api-types/v10';
+import {
+	ModalBuilder,
+	type RestOrArray,
+	type ModalActionRowComponentBuilder,
+} from '@discordjs/builders';
+import type {
+	APIActionRowComponent,
+	APIModalActionRowComponent,
+	APIModalInteractionResponseCallbackData,
+} from 'discord-api-types/v10';
 
 import BaseBuilder from './BaseBuilder';
+import ActionRow from './ActionRowBuilder';
 
 /**
  * Represents a modal dialog component in a Discord interaction
@@ -28,5 +37,76 @@ export default class Modal extends BaseBuilder {
 		options?: Partial<APIModalInteractionResponseCallbackData>
 	) {
 		super(new ModalBuilder(options));
+
+		new ModalBuilder().
+	}
+
+	/**
+	 * Methods for managing modal components
+	 */
+	components = {
+		/**
+		 * Sets the components for this modal, replacing any existing ones
+		 * @param comps - The component(s) to set
+		 * @returns This modal instance
+		 */
+		set: (
+			...comps: RestOrArray<
+				| ActionRow<ModalActionRowComponentBuilder>
+				| APIActionRowComponent<APIModalActionRowComponent>
+			>
+		): this => {
+			// @ts-ignore
+			this.builder.setComponents(...comps);
+
+			return this;
+		},
+
+		/**
+		 * Adds one or more components to this modal
+		 * @param comps - The component(s) to add
+		 * @returns This modal instance
+		 */
+		add: (
+			...comps: RestOrArray<
+				| ActionRow<ModalActionRowComponentBuilder>
+				| APIActionRowComponent<APIModalActionRowComponent>
+			>
+		): this => {
+			// @ts-ignore
+			this.builder.addComponents(...comps);
+
+			return this;
+		},
+	};
+
+	/**
+	 * Sets the custom ID of this modal
+	 * @param id - The custom ID to set
+	 * @returns This modal instance
+	 */
+	public setId(id: string): this {
+		this.builder.setCustomId(id);
+
+		return this;
+	}
+
+  /**
+   * Sets the title of this modal
+   * @param title - The title to set
+   * @returns This modal instance
+   */
+  public setTitle(title: string): this {
+    this.builder.setTitle(title);
+
+    return this;
+  }
+
+	/**
+	 * Converts this modal to a JSON object
+	 * @returns The JSON representation of this modal
+	 */
+	public json(): APIModalInteractionResponseCallbackData {
+		return this.builder.toJSON();
 	}
 }
